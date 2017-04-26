9b671b6665fdb5acc60cc93e9130bd0b1338469f
striplofly <fatinnurliyana8@gmail.com>
2017-04-26 18:34:16 +0800

    First Commit

---
+++ Assets/Scripts/AddButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AddButton : MonoBehaviour {
+    
+    // to get gameobject from scene ; can use this or use getcomponent
+    [SerializeField]
+    private Transform puzzleField;
+
+    [SerializeField]
+    private GameObject btn;
+    
+    void Awake()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
+    }
+}
