f4f91a4d6fa30ecf6abb8cc5d1fe9f629f06cad9
Izwan Harris <izwanharris@gmail.com>
2017-04-27 23:35:54 +0800

    Added testing for UI and Script behaviour

---
+++ Assets/Scripts/TestScript2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("In start function of TestScript2");
+	}
+
+	public void One(){
+		Debug.Log ("So this One function was invoked.");
+	}
+
+}
